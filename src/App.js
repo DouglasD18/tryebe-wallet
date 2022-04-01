38ba9813e796e11d4cf3b1644c116b06e0f66dce
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-03-31 23:23:15 -0300

    Resolução dos requisitos 2 e 3.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path="/">
+        <Route exact path="/carteira">
+          <Wallet />
+        </Route>
+        <Route exact path="/">
           <Login />
         </Route>
       </Switch>
